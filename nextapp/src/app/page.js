6d4a07da1839465aeb9554fc745766c721ac01a8
SANDI <sandi@SANDIs-MacBook-Pro.local>
2025-02-17 17:01:36 +0700

    change fe

---
+++ nextapp/src/app/page.js
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center align-items center h-screen max-w-screen-md">
+    <Button>Button</Button>
+    <br/>
+    <AlertDialog>
+  <AlertDialogTrigger>Open</AlertDialogTrigger>
+  <AlertDialogContent>
+    <AlertDialogHeader>
+      <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+      <AlertDialogDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </AlertDialogDescription>
+    </AlertDialogHeader>
+    <AlertDialogFooter>
+      <AlertDialogCancel>Cancel</AlertDialogCancel>
+      <AlertDialogAction>Continue</AlertDialogAction>
+    </AlertDialogFooter>
+  </AlertDialogContent>
+</AlertDialog>
+
+    </div>
+  );
+}
